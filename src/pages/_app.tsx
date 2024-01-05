38264f3f443057a7a5413b0e5c359df3488f8c8a
Giorgi Keburia <keburius@Giorgis-MacBook-Pro.local>
2024-01-05 20:27:31 +0400

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import Header from "@components/components/Header";
 import "@components/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/tre.ico"></link>
+        <link rel="mask-icon" href="/tre.ico"></link>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
